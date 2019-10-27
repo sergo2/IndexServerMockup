880556014317b5a857b9bf0aff84cf63006b82bb
Sergey Yudakov <sergo2@gmail.com>
2019-10-27 15:53:32 +0300

    Split composition and realtime receivers

--- config.py
+++ config.py
@@ -1,5 +1,12 @@
-index_server_state = 'M'
+comp_receiver_state = 'M'
+comp_state_url = '/composition/v1/'
+
+publisher_state = 'O'
+receiver_url = '/realtime/v1/'
+
 server_port = 65156
-save_index_error = False
+save_index_error = True
 save_index_error_code = -900
-save_index_error_msg = 'Test response'
+save_index_error_msg = ' Hello from Index Server'
+
+known_indices = []
--- server.py
+++ server.py
@@ -13,18 +13,27 @@ class S(BaseHTTPRequestHandler):
     def do_GET(self):
         logging.info("GET request,\nPath: %s\n", str(self.path))
         self._set_response()
-        # to do: set server state from HTTP request
-        indexserver_response = form_status_response(index_server_state)
+        if str(self.path) == receiver_url + "get_state":
+            return_state = publisher_state
+            req_type = 'realtime'
+        else:
+            return_state = comp_receiver_state
+            req_type = 'composition'
+        indexserver_response = form_status_response(return_state, req_type)
         self.wfile.write(indexserver_response.encode('utf-8'))
 
     def do_PUT(self):
         content_length = int(self.headers['Content-Length']) # <--- Gets the size of data
         put_data = self.rfile.read(content_length) # <--- Gets the data itself
+        body_str = put_data.decode('utf-8')
         logging.info("PUT request,\nPath: %s\nBody:\n%s\n", str(self.path), put_data.decode('utf-8'))
+        body_dict = json.loads(body_str)
+        index_code = body_dict['data']['composition']['code']        
+        logging.info(index_code)
         self._set_response()
-        indexserver_response = form_version_response(save_index_error, save_index_error_code, save_index_error_msg)
+        indexserver_response = form_composition_response(save_index_error, save_index_error_code, save_index_error_msg)
         self.wfile.write(indexserver_response.encode('utf-8'))
-        dump_json(put_data)
+        dump_json(body_dict, index_code)
         
     def do_POST(self):
         content_length = int(self.headers['Content-Length']) # <--- Gets the size of data
@@ -35,11 +44,12 @@ class S(BaseHTTPRequestHandler):
         self._set_response()
         self.wfile.write("POST request for {}".format(self.path).encode('utf-8'))
 
-def run(server_class=HTTPServer, handler_class=S, port=server_port):
+def run(server_class=HTTPServer, handler_class=S):
     logging.basicConfig(level=logging.INFO)
-    server_address = ('', port)
+    server_address = ('', server_port)
     httpd = server_class(server_address, handler_class)
-    logging.info(f"Starting Index Server mockup service on port {server_port} with the state {index_server_state}\n")
+    logging.info(f"Starting Index Server mockup service on port {server_port} with \
+    the composition receiver state = {comp_receiver_state} and index value publisher state = {publisher_state}\n")
     try:
         httpd.serve_forever()
     except KeyboardInterrupt:
@@ -50,7 +60,11 @@ def run(server_class=HTTPServer, handler_class=S, port=server_port):
 if __name__ == '__main__':
     from sys import argv
 
-    if len(argv) == 2:
-        run(port=int(argv[1]))
-    else:
+    if len(argv) == 3:
+        comp_receiver_state=argv[1]
+        publisher_state=argv[2]
         run()
+    elif len(argv) == 1:
+        run()
+    else:
+        print('Usage: server.py <comp_receiver_state> <comp_receiver_state>\n       server.py')
